eab778fffdee6f89a3d7da6b76275b429dd7b8cf
Stuart Calder <notstuart@gmail.com>
2020-03-06 22:52:12 -0800

    simplify openbsd unveil() usage

--- main.cc
+++ main.cc
@@ -12,26 +12,12 @@
 #include <ssc/crypto/skein.hh>
 #include <ssc/crypto/skein_csprng.hh>
 
-#ifdef __OpenBSD__
-/* Only OpenBSD systems */
-#	include <unistd.h> // Include unistd.h for unveil() and pledge()
-#	ifndef OPENBSD_UNVEIL
-#		define OPENBSD_UNVEIL(path,permissions) \
-		if (unveil( path, permissions ) != 0) \
-			errx( "Failed to unveil()\n" )
-#	else
-#		error 'OPENBSD_UNVEIL Already Defined'
-#	endif /* ifndef OPENBSD_UNVEIL */
-#else
-/* All non-OpenBSD systems */
-#endif /* #ifdef __OpenBSD__*/
-
 using namespace ssc;
 int main (int const argc, char const *argv[])
 {
 
-	OPENBSD_UNVEIL( "/usr", "rx" );
-	OPENBSD_UNVEIL( nullptr, nullptr );
+	_OPENBSD_UNVEIL( "/usr", "rx" );
+	_OPENBSD_UNVEIL( nullptr, nullptr );
 
 	Password_Generator{ argc, argv };
 	return EXIT_SUCCESS;
