3d6ab798cdabddd023165cbbc21e48f22c750b3a
Stuart Calder <notstuart@gmail.com>
2020-03-26 16:01:11 -0700

    remove unnecessary includes

--- main.cc
+++ main.cc
@@ -8,9 +8,6 @@
 
 #include <ssc/general/macros.hh>
 #include <ssc/general/integers.hh>
-#include <ssc/crypto/operations.hh>
-#include <ssc/crypto/skein.hh>
-#include <ssc/crypto/skein_csprng.hh>
 
 using namespace ssc;
 int main (int const argc, char const *argv[])
