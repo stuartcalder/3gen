0339b307f276f29a5fd5b0144c1bbf62876899dc
Stuart Calder <technoglub@protonmail.com>
2020-11-29 01:58:46 -0800

    Standardized initialization practice.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ main (int argc, char ** argv) {
 	SHIM_OPENBSD_UNVEIL ("/usr", "r");
 	SHIM_OPENBSD_UNVEIL (NULL, NULL);
 	SHIM_OPENBSD_PLEDGE ("stdio tty", NULL);
-	Threegen tg = THREEGEN_NULL_INIT;
+	Threegen tg = { 0 };
 	threegen( argc, argv, &tg );
 
 	return EXIT_SUCCESS;
