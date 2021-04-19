c0e94cfab58cf70c52a7f391645155d7c4cdc66e
Stuart Calder <technoglub@protonmail.com>
2021-04-19 03:51:29 -0700

    Simplify source code.

--- args.c
+++ args.c
@@ -14,64 +14,51 @@
 
 Shim_Arg_Handler_f *
 short_parser (char const * str) {
-	size_t const str_size = strlen( str );
-	switch( str_size ) {
+	size_t const str_size = strlen(str);
+	switch (str_size) {
 		case 2:
-			switch( str[ 1 ] ) {
-				case 'h':
-					return h_handler;
-				case 'l':
-					return l_handler;
-				case 'u':
-					return u_handler;
-				case 'd':
-					return d_handler;
-				case 's':
-					return s_handler;
-				case 'f':
-					return f_handler;
-				case 'a':
-					return a_handler;
-				case 'E':
-					return E_handler;
+			switch (str[1]) {
+				case 'h': return h_handler;
+				case 'l': return l_handler;
+				case 'u': return u_handler;
+				case 'd': return d_handler;
+				case 's': return s_handler;
+				case 'f': return f_handler;
+				case 'a': return a_handler;
+				case 'E': return E_handler;
 			}
 	}
 	HANDLE_INVALID_ARG_ (str);
 	return NULL;
 }
 
+#define STR_EQ_(s0, s1) (!strcmp(s0, s1))
+#define STR_TO_F_(s, f) if (STR_EQ_(str + 2, s)) return f
+
 Shim_Arg_Handler_f *
 long_parser (char const * str) {
-	size_t const str_size = strlen( str );
-	switch( str_size ) {
+	size_t const str_size = strlen(str) - 2;
+	switch (str_size) {
+		case 3:
+			STR_TO_F_("all", all_handler);
+			break;
+		case 4:
+			STR_TO_F_("help", help_handler);
+			break;
 		case 5:
-			if( strcmp( str, "--all" ) == 0 )
-				return all_handler;
+			STR_TO_F_("lower", lower_handler);
+			STR_TO_F_("upper", upper_handler);
+			STR_TO_F_("digit", digit_handler);
 			break;
 		case 6:
-			if( strcmp( str, "--help" ) == 0 )
-				return help_handler;
+			STR_TO_F_("symbol", symbol_handler);
+			STR_TO_F_("format", format_handler);
 			break;
 		case 7:
-			if( strcmp( str, "--lower" ) == 0 )
-				return lower_handler;
-			if( strcmp( str, "--upper" ) == 0 )
-				return upper_handler;
-			if( strcmp( str, "--digit" ) == 0 )
-				return digit_handler;
-			break;
-		case 8:
-			if( strcmp( str, "--symbol" ) == 0 )
-				return symbol_handler;
-			if( strcmp( str, "--format" ) == 0 )
-				return format_handler;
-			break;
-		case 9:
-			if( strcmp( str, "--entropy" ) == 0 )
-				return entropy_handler;
+			STR_TO_F_("entropy", entropy_handler);
 			break;
 	}
-	HANDLE_INVALID_ARG_ (str);
+	HANDLE_INVALID_ARG_(str);
 	return NULL;
 }
 
@@ -82,12 +69,10 @@ floating_parser (char const * discard) {
 
 Shim_Arg_Parser_f *
 arg_processor (char const * str, void * SHIM_RESTRICT v_ctx) {
-	int type = shim_argtype( str );
-	switch( type ) {
-		case SHIM_ARGTYPE_SHORT:
-			return short_parser;
-		case SHIM_ARGTYPE_LONG:
-			return long_parser;
+	int type = shim_argtype(str);
+	switch (type) {
+		case SHIM_ARGTYPE_SHORT: return short_parser;
+		case SHIM_ARGTYPE_LONG:  return long_parser;
 	}
 	return floating_parser;
 }
@@ -97,68 +82,34 @@ arg_processor (char const * str, void * SHIM_RESTRICT v_ctx) {
 	name##_handler (char ** str_arr, int const str_cnt, void * SHIM_RESTRICT v_ctx)
 #define CTX_ ((Threegen *)v_ctx)
 
-HANDLER_IMPL_ (h) {
-	print_help();
-	exit( EXIT_SUCCESS );
-}
-
-HANDLER_IMPL_ (l) {
-	CTX_->flags |= THREEGEN_USE_LCASE;
-}
-
-HANDLER_IMPL_ (u) {
-	CTX_->flags |= THREEGEN_USE_UCASE;
-}
-
-HANDLER_IMPL_ (d) {
-	CTX_->flags |= THREEGEN_USE_DIGITS;
-}
-
-HANDLER_IMPL_ (s) {
-	CTX_->flags |= THREEGEN_USE_SYMBOLS;
-}
-
-#define USE_ALL_CHARS_ (THREEGEN_USE_LCASE   | \
-			THREEGEN_USE_UCASE   | \
-			THREEGEN_USE_DIGITS  | \
-			THREEGEN_USE_SYMBOLS)
-
-HANDLER_IMPL_ (a) {
-	CTX_->flags |= USE_ALL_CHARS_;
-}
-
-HANDLER_IMPL_ (f) {
-	CTX_->flags |= THREEGEN_USE_FORMATTING;
-}
-
-HANDLER_IMPL_ (E) {
-	CTX_->flags |= THREEGEN_GET_ENTROPY;
-}
+HANDLER_IMPL_(h) { print_help(); exit(EXIT_SUCCESS); }
+HANDLER_IMPL_(l) { CTX_->flags |= THREEGEN_USE_LCASE; }
+HANDLER_IMPL_(u) { CTX_->flags |= THREEGEN_USE_UCASE; }
+HANDLER_IMPL_(d) { CTX_->flags |= THREEGEN_USE_DIGITS; }
+HANDLER_IMPL_(s) { CTX_->flags |= THREEGEN_USE_SYMBOLS; }
+#define USE_ALL_CHARS_ (THREEGEN_USE_LCASE|THREEGEN_USE_UCASE|THREEGEN_USE_DIGITS|THREEGEN_USE_SYMBOLS)
+HANDLER_IMPL_(a) { CTX_->flags |= USE_ALL_CHARS_; }
+HANDLER_IMPL_(f) { CTX_->flags |= THREEGEN_USE_FORMATTING; }
+HANDLER_IMPL_(E) { CTX_->flags |= THREEGEN_GET_ENTROPY; }
 
 #define ERR_MIN_PW_SIZE_PROMPT_ "Error: Minimum password size is 1 character.\n"
 #define ERR_MAX_PW_SIZE_PROMPT_ "Error: Maximum password size is " STRINGIFY_ (THREEGEN_MAX_PW_SIZE) " characters.\n"
 
-HANDLER_IMPL_ (password_size) {
+HANDLER_IMPL_(password_size) {
 	char const * str = *str_arr;
-	size_t const str_size = strlen( str );
-	if( str_size < 1 )
-		shim_errx(ERR_MIN_PW_SIZE_PROMPT_);
-	if( str_size > 3 )
-		shim_errx(ERR_MAX_PW_SIZE_PROMPT_);
-	char * scratch_str = (char *)shim_enforce_malloc( str_size + 1 );
+	size_t const str_size = strlen(str);
+	shim_assert_msg(str_size >= 1, ERR_MIN_PW_SIZE_PROMPT_);
+	shim_assert_msg(str_size <= 3, ERR_MAX_PW_SIZE_PROMPT_);
+	char * scratch_str = (char *)shim_enforce_malloc(str_size + 1);
 	{
-		memcpy( scratch_str, str, (str_size + 1) );
-		int num_digits = shim_shift_left_digits( scratch_str, str_size );
-		if( num_digits < 1 )
-			shim_errx(ERR_MIN_PW_SIZE_PROMPT_);
-		if( num_digits > 3 )
-			shim_errx(ERR_MAX_PW_SIZE_PROMPT_);
-		int size = atoi( scratch_str );
-		if( size < 1 )
-			shim_errx(ERR_MIN_PW_SIZE_PROMPT_);
-		if( size > THREEGEN_MAX_PW_SIZE )
-			shim_errx(ERR_MAX_PW_SIZE_PROMPT_);
+		memcpy(scratch_str, str, str_size + 1);
+		int num_digits = shim_shift_left_digits(scratch_str, str_size);
+		shim_assert_msg(num_digits >= 1, ERR_MIN_PW_SIZE_PROMPT_);
+		shim_assert_msg(num_digits <= 3, ERR_MAX_PW_SIZE_PROMPT_);
+		int size = atoi(scratch_str);
+		shim_assert_msg(size >= 1, ERR_MIN_PW_SIZE_PROMPT_);
+		shim_assert_msg(size <= THREEGEN_MAX_PW_SIZE, ERR_MAX_PW_SIZE_PROMPT_);
 		CTX_->requested_pw_size = size;
 	}
-	free( scratch_str );
+	free(scratch_str);
 }
--- threegen.c
+++ threegen.c
@@ -24,51 +24,50 @@ set_character_table (Threegen * ctx) {
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 	};
-	SHIM_STATIC_ASSERT (sizeof(Lowercase_Set) == THREEGEN_NUM_LCASE, "Set size mismatch.");
+	SHIM_STATIC_ASSERT(sizeof(Lowercase_Set) == THREEGEN_NUM_LCASE, "Set size mismatch.");
 	static uint8_t const Uppercase_Set[] = {
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 	};
-	SHIM_STATIC_ASSERT (sizeof(Uppercase_Set) == THREEGEN_NUM_UCASE, "Set size mismatch.");
+	SHIM_STATIC_ASSERT(sizeof(Uppercase_Set) == THREEGEN_NUM_UCASE, "Set size mismatch.");
 	static uint8_t const Digit_Set[] = {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 	};
-	SHIM_STATIC_ASSERT (sizeof(Digit_Set) == THREEGEN_NUM_DIGITS, "Set size mismatch.");
+	SHIM_STATIC_ASSERT(sizeof(Digit_Set) == THREEGEN_NUM_DIGITS, "Set size mismatch.");
 	static uint8_t const Symbol_Set[] = {
 		'!', '"', '#', '$','%', '&', '\'', '(', ')', '*',
 		'+', ',', '-', '.', '/', ':', ';', '<', '=', '>',
 		'?', '@', '[', '\\', ']', '^', '_', '`', '{', '|',
 		'}', '~'
 	};
-	SHIM_STATIC_ASSERT (sizeof(Symbol_Set) == THREEGEN_NUM_SYMBOLS, "Set size mismatch.");
+	SHIM_STATIC_ASSERT(sizeof(Symbol_Set) == THREEGEN_NUM_SYMBOLS, "Set size mismatch.");
 	uint8_t * character = ctx->character_table;
 	bool one_is_valid = false;
-	if( ctx->flags & THREEGEN_USE_LCASE ) {
-		memcpy( character, Lowercase_Set, sizeof(Lowercase_Set) );
+	if (ctx->flags & THREEGEN_USE_LCASE) {
+		memcpy(character, Lowercase_Set, sizeof(Lowercase_Set));
 		character += sizeof(Lowercase_Set);
 		ctx->num_chars += sizeof(Lowercase_Set);
 		one_is_valid = true;
 	}
-	if( ctx->flags & THREEGEN_USE_UCASE ) {
-		memcpy( character, Uppercase_Set, sizeof(Uppercase_Set) );
+	if (ctx->flags & THREEGEN_USE_UCASE) {
+		memcpy(character, Uppercase_Set, sizeof(Uppercase_Set));
 		character += sizeof(Uppercase_Set);
 		ctx->num_chars += sizeof(Uppercase_Set);
 		one_is_valid = true;
 	}
-	if( ctx->flags & THREEGEN_USE_DIGITS ) {
-		memcpy( character, Digit_Set, sizeof(Digit_Set) );
+	if (ctx->flags & THREEGEN_USE_DIGITS) {
+		memcpy(character, Digit_Set, sizeof(Digit_Set));
 		character += sizeof(Digit_Set);
 		ctx->num_chars += sizeof(Digit_Set);
 		one_is_valid = true;
 	}
-	if( ctx->flags & THREEGEN_USE_SYMBOLS ) {
-		memcpy( character, Symbol_Set, sizeof(Symbol_Set) );
+	if (ctx->flags & THREEGEN_USE_SYMBOLS) {
+		memcpy(character, Symbol_Set, sizeof(Symbol_Set));
 		character += sizeof(Symbol_Set);
 		ctx->num_chars += sizeof(Symbol_Set);
 		one_is_valid = true;
 	}
-	if( !one_is_valid )
-		shim_errx("Error: No valid character sets chosen. Use -h for help.\n");
+	shim_assert_msg(one_is_valid, "Error: No valid character sets chosen. Use -h for help.\n");
 }
 
 #if    defined (SHIM_OS_UNIXLIKE)
@@ -79,13 +78,13 @@ set_character_table (Threegen * ctx) {
 #	error "Unsupported OS."
 #endif
 
-#define STRINGIFY_IMPL_(text) \
+#define STR_IMPL_(text) \
 	#text
-#define STRINGIFY_(text) \
-	STRINGIFY_IMPL_ (text)
+#define STR_(text) \
+	STR_IMPL_ (text)
 
 #define ENT_PROMPT_ "Please input up to " \
-		    STRINGIFY_ (THREEGEN_MAX_ENT_SIZE) \
+		    STR_(THREEGEN_MAX_ENT_SIZE) \
 		    " random characters." PROMPT_
 
 static void
@@ -95,17 +94,17 @@ supplement_entropy_ (Symm_CSPRNG * SHIM_RESTRICT csprng,
 	uint8_t * hash = buffer;
 	uint8_t * keyboard_input = hash + SYMM_THREEFISH512_BLOCK_BYTES;
 	shim_term_init();
-	int num_input_chars = shim_term_obtain_password( keyboard_input,
-							 ENT_PROMPT_,
-							 1,
-							 THREEGEN_MAX_ENT_SIZE,
-							 (THREEGEN_MAX_ENT_SIZE + 1) );
+	int num_input_chars = shim_term_obtain_password(keyboard_input,
+							ENT_PROMPT_,
+							1,
+							THREEGEN_MAX_ENT_SIZE,
+							(THREEGEN_MAX_ENT_SIZE + 1));
 	shim_term_end();
-	symm_skein512_hash_native( &csprng->ubi512_ctx,
-				   hash,
-				   keyboard_input,
-				   num_input_chars );
-	symm_csprng_reseed( csprng, hash );
+	symm_skein512_hash_native(&csprng->ubi512_ctx,
+				  hash,
+				  keyboard_input,
+				  num_input_chars);
+	symm_csprng_reseed(csprng, hash);
 }
 
 static size_t
@@ -115,20 +114,20 @@ generate_password_ (Threegen *       SHIM_RESTRICT ctx,
 {
 	uint64_t const local_limit = THREEGEN_UPPER_LIMIT - (THREEGEN_UPPER_LIMIT % ((uint64_t)ctx->num_chars));
 	uint64_t const quanta_per_char = local_limit / ((uint64_t)ctx->num_chars); /* The number of integers per each character. */
-	memset( pw, 0, THREEGEN_PW_BUF_SIZE );
+	memset(pw, 0, THREEGEN_PW_BUF_SIZE);
 	int const requested_pw_size = ctx->requested_pw_size;
-	for( int i = 0; i < requested_pw_size; ++i ) {
+	for (int i = 0; i < requested_pw_size; ++i) {
 		uint64_t offset;
-		uint64_t p = rand_words[ i ];
-		if( p <= local_limit ) {
+		uint64_t p = rand_words[i];
+		if (p <= local_limit) {
 			uint64_t const p_prime = p - (p % quanta_per_char);
 			offset = p_prime / quanta_per_char;
 		} else {
 			offset = ctx->num_chars - 1;
 		}
-		pw[ i ] = ctx->character_table[ offset ];
+		pw[i] = ctx->character_table[offset];
 	}
-	return strlen( (char *)pw );
+	return strlen((char *)pw);
 }
 
 void
@@ -136,17 +135,17 @@ threegen (int argc, char ** argv,
 	  Threegen * SHIM_RESTRICT ctx)
 {
 	Crypto crypto;
-	symm_csprng_init( &crypto.csprng );
-	shim_process_args( argc, argv, arg_processor, ctx );
-	set_character_table( ctx );
-	if( ctx->flags & THREEGEN_GET_ENTROPY )
-		supplement_entropy_( &crypto.csprng, crypto.ent_bytes );
+	symm_csprng_init(&crypto.csprng);
+	shim_process_args(argc, argv, arg_processor, ctx);
+	set_character_table(ctx);
+	if (ctx->flags & THREEGEN_GET_ENTROPY)
+		supplement_entropy_(&crypto.csprng, crypto.ent_bytes);
 	SHIM_OPENBSD_PLEDGE ("stdio tty", NULL);
-	symm_csprng_get( &crypto.csprng,
-			 (uint8_t *)crypto.rand_bytes,
-			 sizeof(crypto.rand_bytes) );
-	int const size = generate_password_( ctx, crypto.passwd, crypto.rand_bytes );
-	if( ctx->flags & THREEGEN_USE_FORMATTING ) {
+	symm_csprng_get(&crypto.csprng,
+			(uint8_t *)crypto.rand_bytes,
+			sizeof(crypto.rand_bytes));
+	int const size = generate_password_(ctx, crypto.passwd, crypto.rand_bytes);
+	if (ctx->flags & THREEGEN_USE_FORMATTING) {
 		enum {
 			CHARS_PER_BLOCK_ = 5,
 			BLOCKS_PER_LINE_ = 5
@@ -154,25 +153,25 @@ threegen (int argc, char ** argv,
 		int chars_left = size;
 		int blocks_left = BLOCKS_PER_LINE_;
 		uint8_t * pw = crypto.passwd;
-		while( chars_left >= CHARS_PER_BLOCK_ ) {
-			fwrite( pw, 1, CHARS_PER_BLOCK_, stdout );
+		while (chars_left >= CHARS_PER_BLOCK_) {
+			fwrite(pw, 1, CHARS_PER_BLOCK_, stdout);
 			pw         += CHARS_PER_BLOCK_;
 			chars_left -= CHARS_PER_BLOCK_;
-			if( --blocks_left == 0 ) {
+			if (!(--blocks_left)) {
 				blocks_left = BLOCKS_PER_LINE_;
-				putchar( '\n' );
+				putchar('\n');
 			} else {
-				fputs( "  ", stdout );
+				fputs("  ", stdout);
 			}
 		}
-		if( chars_left > 0 )
-			fwrite( pw, 1, chars_left, stdout );
-		if( blocks_left != BLOCKS_PER_LINE_ )
-			putchar( '\n' );
+		if (chars_left)
+			fwrite(pw, 1, chars_left, stdout);
+		if (blocks_left != BLOCKS_PER_LINE_)
+			putchar('\n');
 	} else {
-		fwrite( crypto.passwd, 1, size, stdout );
-		putchar( '\n' );
+		fwrite(crypto.passwd, 1, size, stdout);
+		putchar('\n');
 	}
-	shim_secure_zero( &crypto, sizeof(crypto) );
-	shim_secure_zero( ctx, sizeof(*ctx) );
+	shim_secure_zero(&crypto, sizeof(crypto));
+	shim_secure_zero(ctx, sizeof(*ctx));
 }
